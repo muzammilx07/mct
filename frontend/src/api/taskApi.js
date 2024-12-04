e42ab0eee36ace65322e86ad8689ad6b269ce4f5
Muzammil Shareef <muzammilxshareef@gmail.com>
2024-12-05 02:00:56 +0530

    2 commit

--- frontend/src/api/taskApi.js
+++ frontend/src/api/taskApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/tasks';
+const API_URL = "https://mct-fxcs.onrender.com/tasks";
 
 export const fetchTasks = async () => {
   try {
